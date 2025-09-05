e26565b8aceb30f3291d987083d759eca38a7a74
Naod Solomon <naodsolomon8911@gmail.com>
2025-09-05 03:34:02 +0300

    Finished Integrating REST API with Express

--- src/app.js
+++ src/app.js
@@ -4,3 +4,11 @@ mongoose.connect("mongodb://127.0.0.1:27017/todoapp", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use('/tasks', require('./routes.taskRoutes'));
+
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
 //My node file
+
+const http = require('http');
+const app = require('./app');
+const { initSocket } = require('./sockets/socket');
+
+const server = http.createserver(app);
+initSocket(server);
+
+server.listen(4000, () => console.log('Server running on port 4000'));
